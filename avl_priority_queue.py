51eb185753a1aabdd750c3de8228fe08dfdb9e76
Timur <pelmar223@gmail.com>
2026-03-22 11:36:07 +0200

    1

--- avl_priority_queue.py
+++ avl_priority_queue.py
@@ -1,5 +1,4 @@
 class Node:
-    """Вузол AVL-дерева, що зберігає значення та пріоритет."""
     def __init__(self, value, priority):
         self.value = value
         self.priority = priority
@@ -9,7 +8,6 @@ class Node:
 
 
 class AVLPriorityQueue:
-    """Черга з пріоритетами на основі AVL-дерева."""
     def __init__(self):
         self.root = None
 
@@ -27,11 +25,9 @@ class AVLPriorityQueue:
         x = y.left
         T2 = x.right
 
-        # Виконуємо поворот
         x.right = y
         y.left = T2
 
-        # Оновлюємо висоти
         y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
         x.height = 1 + max(self._get_height(x.left), self._get_height(x.right))
 
@@ -41,18 +37,15 @@ class AVLPriorityQueue:
         y = x.right
         T2 = y.left
 
-        # Виконуємо поворот
         y.left = x
         x.right = T2
 
-        # Оновлюємо висоти
         x.height = 1 + max(self._get_height(x.left), self._get_height(x.right))
         y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
 
         return y
 
     def insert(self, value, priority):
-        """1. Вставка елемента з заданим значенням та пріоритетом."""
         self.root = self._insert_node(self.root, value, priority)
 
     def _insert_node(self, root, value, priority):
